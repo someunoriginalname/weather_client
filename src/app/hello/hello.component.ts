7abb289e77b1cf4bd7461287f43f15d1fddc9edb
someunoriginalname <chilians@yahoo.com>
2024-02-16 20:42:15 -0800

    added hello

---
+++ src/app/hello/hello.component.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+interface WeatherForecast {
+  date: string;
+  temperatureC: number;
+  temperatureF: number;
+  summary: string;
+}
+
+@Component({
+  selector: 'app-hello',
+  standalone: true,
+  imports: [],
+  templateUrl: './hello.component.html',
+  styleUrl: './hello.component.scss'
+})
+export class HelloComponent implements OnInit{
+  public forecasts: WeatherForecast[] = [];
+  baseUrl = "http://localhost:5263/";
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    this.getForecasts();
+  }
+  getForecasts() {
+    this.http.get<WeatherForecast[]>(this.baseUrl + 'weatherforecast').subscribe(
+      (result) => {
+        this.forecasts = result;
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
+  title = 'weatherclient';
+}
