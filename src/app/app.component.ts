68e7e5d1f03caab4cd1fc1ee87f9b21632b7c30e
someunoriginalname <chilians@yahoo.com>
2024-02-23 20:31:12 -0800

    Dashbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HelloComponent } from "./hello/hello.component";
 import { HttpClientModule } from '@angular/common/http';
+import { NavBarComponent } from "./nav-bar/nav-bar.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, HelloComponent, HttpClientModule]
+    imports: [RouterOutlet, HelloComponent, HttpClientModule, NavBarComponent]
 })
 export class AppComponent {
   title = 'my-app';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { HelloComponent } from './hello/hello.component';
+import { CountriesComponent } from './countries/countries.component';
+export const routes: Routes = [{path:'', component:HelloComponent, pathMatch: 'full'}, {path:'countries',component:CountriesComponent}];
 
-export const routes: Routes = [];
